3b8da694eb180df3b24ecce4e03ee939c05a023d
Rasul Kireev <me@rasulkireev.com>
2024-09-27 14:25:00 +0300

    add posthog and plausinle

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,11 @@
+import posthog
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        posthog.api_key = "phc_wa2RfooG5BAVE9zHHGkeEyF52nJTbwmcjdjrH32ZKl6"
+        posthog.host = "https://us.i.posthog.com"
