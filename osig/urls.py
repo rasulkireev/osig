ac26355c6b8048b75284544729591b7ed404c1b5
Rasul Kireev <me@rasulkireev.com>
2024-10-27 13:00:13 +0300

    uses + status

--- osig/urls.py
+++ osig/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from osig.sitemaps import sitemaps
 
@@ -24,6 +25,7 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("anymail/", include("anymail.urls")),
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
+    path("uses", TemplateView.as_view(template_name="pages/uses.html"), name="uses"),
     path("", include("core.urls")),
     path(
         "sitemap.xml",
